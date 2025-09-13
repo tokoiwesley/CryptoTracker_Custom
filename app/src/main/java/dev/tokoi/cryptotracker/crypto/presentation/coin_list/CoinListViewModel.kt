d0e61303daecc6310b31e1a800fcce4495ba318c
Wesley Tokoi <tokoiwesley@gmail.com>
2025-09-14 02:50:23 +0300

    feat: add network error handling

--- app/src/main/java/dev/tokoi/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
+++ app/src/main/java/dev/tokoi/cryptotracker/crypto/presentation/coin_list/CoinListViewModel.kt
@@ -6,9 +6,11 @@ import dev.tokoi.cryptotracker.core.domain.util.onError
 import dev.tokoi.cryptotracker.core.domain.util.onSuccess
 import dev.tokoi.cryptotracker.crypto.domain.CoinDataSource
 import dev.tokoi.cryptotracker.crypto.presentation.models.toCoinUi
+import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.onStart
+import kotlinx.coroutines.flow.receiveAsFlow
 import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -24,6 +26,9 @@ class CoinListViewModel(
             SharingStarted.WhileSubscribed(5000L),
             CoinListState()
         )
+
+    private val _events = Channel<CoinListEvent>()
+    val events = _events.receiveAsFlow()
 
     fun onAction(action: CoinListAction) {
         when (action) {
@@ -56,6 +61,7 @@ class CoinListViewModel(
                 }
                 .onError { error ->
                     _state.update { it.copy(isLoading = false) }
+                    _events.send(CoinListEvent.Error(error))
                 }
         }
     }
